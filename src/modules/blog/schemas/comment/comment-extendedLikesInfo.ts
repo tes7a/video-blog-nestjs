1bb78e2c81d78f986e5ac67f76f566dfb1fd14de
Konstantin <ttes7a@gmail.com>
2025-05-04 16:25:58 +0300

    fixed schema

--- src/modules/blog/schemas/comment/comment-extendedLikesInfo.ts
+++ src/modules/blog/schemas/comment/comment-extendedLikesInfo.ts
@@ -12,7 +12,7 @@ export class CommentExtendedLikesInfo {
   @Prop()
   myStatus: 'None' | 'Like' | 'Dislike';
 
-  @Prop({ type: UserRatingsSchema })
+  @Prop({ type: [UserRatingsSchema] })
   userRatings?: Array<UserRatings>;
 }
 
