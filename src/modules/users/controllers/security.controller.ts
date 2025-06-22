c2a915d4d2dc0ade2e5258db3441f25a8138d97a
Konstantin <ttes7a@gmail.com>
2025-06-22 16:46:48 +0300

    fixes

--- src/modules/users/controllers/security.controller.ts
+++ src/modules/users/controllers/security.controller.ts
@@ -8,50 +8,41 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { DeviceRepository } from '../infrastructure';
-import { JwtAuthGuard } from '../guards';
-import { CurrentUser } from '../decorators';
-import { UserType } from '../models';
+import { JwtRefreshGuard } from '../guards';
+import { Tokens } from '../decorators';
 import { Response } from 'express';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtRefreshGuard)
 @Controller('/security')
 export class SecurityController {
   constructor(private deviceRepository: DeviceRepository) {}
 
   @Get('/devices')
   async getAllDevices(
-    @CurrentUser()
-    accountData: Pick<UserType, 'id'> &
-      Pick<UserType['accountData'], 'email' | 'login'>,
+    @Tokens()
+    data: Tokens & { userId: string; deviceId: string },
   ) {
-    const devices = await this.deviceRepository.getAllDevices(accountData.id);
+    const devices = await this.deviceRepository.getAllDevices(data.userId);
     return devices;
   }
 
   @Delete('/devices')
   async deleteAllDevices(
-    @CurrentUser()
-    accountData: Pick<UserType, 'id'> &
-      Pick<UserType['accountData'], 'email' | 'login'> & { deviceId: string },
+    @Tokens() data: Tokens & { userId: string; deviceId: string },
     @Res() response: Response,
   ) {
-    await this.deviceRepository.deleteAllDevices(
-      accountData.id,
-      accountData.deviceId,
-    );
+    await this.deviceRepository.deleteAllDevices(data.userId, data.deviceId);
     return response.sendStatus(HttpStatus.NO_CONTENT);
   }
 
   @Delete('/devices/:deviceId')
   async deleteCurrentDevice(
-    @CurrentUser()
-    accountData: Pick<UserType, 'id'> &
-      Pick<UserType['accountData'], 'email' | 'login'>,
+    @Tokens() data: Tokens & { userId: string; deviceId: string },
     @Param('deviceId') deviceId: string,
     @Res() response: Response,
   ) {
     if (!deviceId) return response.sendStatus(HttpStatus.NOT_FOUND);
-    await this.deviceRepository.deleteCurrentDevice(accountData.id, deviceId);
+    await this.deviceRepository.deleteCurrentDevice(data.userId, deviceId);
     return response.sendStatus(HttpStatus.NO_CONTENT);
   }
 }
